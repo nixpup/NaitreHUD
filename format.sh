7dd176a3caf2ccaa868d06ccf9b1f78d49cef8cb
nixpup <zweitlaender@gmail.com>
2026-01-22 17:59:45 +0100

    Updated .scm File and Removed Unnecessary Files

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-clang-format -i src/*/*.h -i  src/*/*.c -i  src/mango.c -i mmsg/mmsg.c -i mmsg/arg.h -i mmsg/dynarr.h 
+clang-format -i src/*/*.h -i  src/*/*.c -i  src/naitre.c -i hud-msg/hud-msg.c -i hud-msg/arg.h -i hud-msg/dynarr.h 
